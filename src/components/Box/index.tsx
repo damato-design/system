0736f4ad182f46a90d436e6312b6a56600b25e8c
fauxserious <fauxserious@gmail.com>
2024-09-29 15:44:37 +0000

    Build storybook

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -2,11 +2,11 @@ import React from 'react';
 import styles from './styles.module.css';
 
 interface ComponentProps extends React.ComponentProps<'div'> {
-
-}
+  children: string;
+};
 
 function Box(props: ComponentProps) {
-  return <div className={styles.box} {...props} />;
+  return <div {...props} className={styles.box} />;
 }
 
 export default Box;
