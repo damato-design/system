d2b1e333a96ef1c47483d25dd87756eaed621531
Donnie D'Amato <fauxserious@gmail.com>
2024-09-30 15:00:11 -0400

    interfacing

--- src/components/Box/index.tsx
+++ src/components/Box/index.tsx
@@ -2,37 +2,44 @@ import React from 'react';
 import clsx from 'clsx';
 import styles from './styles.module.css';
 
-export type boxProps = React.ComponentProps<any> & {
+interface BoxComponentProps {
   /**
    * Determines if the component should show a loading state
    */
   standby?: boolean;
+}
+
+interface BoxComponent extends React.FC<BoxComponentProps> {
+  displayName: string;
+}
+
+type DynamicProxy = {
+  [key: string]: BoxComponent;
 };
 
-type boxComponent = (tagName: string) => React.FC<boxProps>;
+const createBox = (tagName: string) => {
+  const Box: BoxComponent = ({
+    standby,
+    className,
+    style,
+    ...rest
+  }: BoxComponentProps): JSX.Element => {
+    // Render element
+    const Tag = tagName as keyof JSX.IntrinsicElements;
+    return <Tag { ...rest } className={ clsx(styles.box) } data-standby={ standby } />
+  } 
+  Box.displayName = `box.${tagName}`;
+  return Box as BoxComponent;
+}
 
-const tagProxy = (fn: boxComponent) => {
+const tagProxy = () => {
   const cache = new Map();
-  return new Proxy(fn, {
+  return new Proxy({}, {
     get(_, tagName: string) {
-      if (!cache.has(tagName)) cache.set(tagName, fn(tagName));
+      if (!cache.has(tagName)) cache.set(tagName, createBox(tagName));
       return cache.get(tagName);
     }
   })
 }
 
-const createBox: boxComponent = (TagName) => {
-  const Box = ({
-    standby,
-    className,
-    style,
-    ...rest
-  }: boxProps): JSX.Element => {
-    // Render element
-    return <TagName { ...rest } className={ clsx(styles.box) } data-standby={ standby } />
-  } 
-  Box.displayName = `box.${TagName}`;
-  return Box;
-}
-
-export const box = tagProxy(createBox);
+export const box: DynamicProxy = tagProxy();
