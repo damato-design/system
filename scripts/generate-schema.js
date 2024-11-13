c89d2116da61db9559d89c701df03c350760a207
Donnie D'Amato <fauxserious@gmail.com>
2024-11-13 08:08:52 -0500

    Strict schema

--- scripts/generate-schema.js
+++ scripts/generate-schema.js
@@ -9,18 +9,28 @@ import {
 const schema = {
     $schema: 'http://json-schema.org/draft-07/schema#',
     type: 'object',
+    required: ['tokens'],
     properties: {
-        ...permutate(['surface'], PRIORITY, PROPERTY_COLOR),
-        ...permutate(['action'], PRIORITY, PROPERTY_COLOR),
-        ...permutate(['control'], PROPERTY_COLOR),
-        ...permutate(['text'], PRIORITY, PROPERTY_FONT),
-        ...permutate(['space'], PROPERTY_SPACE),
+        tokens: {
+            type: 'object',
+            required: ['surface', 'action', 'control', 'text', 'space'],
+            properties: {
+                ...permutate(['surface'], PRIORITY, PROPERTY_COLOR),
+                ...permutate(['action'], PRIORITY, PROPERTY_COLOR),
+                ...permutate(['control'], PROPERTY_COLOR),
+                ...permutate(['text'], PRIORITY, PROPERTY_FONT),
+                ...permutate(['space'], PROPERTY_SPACE),
+            }
+            
+        }
     }
 }
 
 function final() {
     return {
         type: 'object',
+        additionalProperties: false,
+        required: ['$value'],
         properties: {
             $value: {
                 type: ['number', 'string']
@@ -41,6 +51,10 @@ function permutate(...arrays) {
             acc[key] = final();
             if (rest.length) {
                 acc[key].type = 'object';
+                acc[key].additionalProperties = false;
+                if (rest[0]) {
+                    acc[key].required = rest[0];
+                }
                 acc[key].properties = {};
                 recurse(acc[key].properties, rest);
             }
