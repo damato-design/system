449f12a9eff1139f4f1e895856f5a4d7beb5569e
fauxserious <fauxserious@gmail.com>
2024-11-19 20:47:40 +0000

    Create font size vars

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -2,18 +2,21 @@ import css from './styles.module.scss';
 import { proxy, Props } from '../Element/proxy';
 
 type IconProps = Props & {
-  standby?: boolean
+  standby?: boolean,
+  priority?: 'primary' | 'secondary'
 }
 
 export const icon = proxy<string, IconProps>('icon', (reference) => {
   return ({
     standby = true,
+    priority,
     ...props
   }: IconProps) => {
     return (
       <i
         { ...props }
         role='presentation'
+        data-priority={ priority }
         data-standby={ standby }
         className={ css.icon }>
         { reference }
