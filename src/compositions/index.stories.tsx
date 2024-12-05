d4cb260eba19bfddccc73d9b64794d66ded06548
Donnie D'Amato <fauxserious@gmail.com>
2024-12-05 18:17:26 -0500

    Cleanup pages

--- src/compositions/index.stories.tsx
+++ src/compositions/index.stories.tsx
@@ -1,8 +1,8 @@
 import type { Meta, StoryObj } from '@storybook/react'
 
-import { ProductDetailsPage as PDP } from './product-details-page';
-import { ProductListingPage as PLP } from './product-listing-page';
-import { PricingPage as PP } from './pricing-page';
+import { ProductDetailsPage } from './product-details-page';
+import { ProductListingPage } from './product-listing-page';
+import { PricingPage } from './pricing-page';
 
 const Placeholder = () => null;
 
@@ -13,19 +13,19 @@ const meta = {
     parameters: {
         densityLevel: false
     }
-} satisfies Meta<typeof PDP>
+} satisfies Meta<typeof Placeholder>
 
 export default meta
 type Story = StoryObj<typeof meta>
 
-export const ProductDetailsPage: Story = {
-    render: PDP
+export const ProductDetails: Story = {
+    render: ProductDetailsPage
 }
 
-export const ProductListingPage: Story = {
-    render: PLP
+export const ProductListing: Story = {
+    render: ProductListingPage
 }
 
-export const PricingPage: Story = {
-    render: PP
+export const Pricing: Story = {
+    render: PricingPage
 }
