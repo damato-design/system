f555c4a66420365e107e70df547942b176403a5b
Donnie D'Amato <fauxserious@gmail.com>
2025-02-05 14:17:34 -0500

    Button jsdoc

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,12 +1,18 @@
-import { createElement, forwardRef } from 'react';
+import { createElement, forwardRef, ReactElement, ReactNode } from 'react';
 import { box, BoxProps } from '../Box';
 import { icon } from '../Icon';
 import { IDREF } from '../Localize';
 import { useFlyout } from '../Flyout';
 import { useListbox } from '../Listbox';
 
-type Accessory = 'menu' | 'external' | 'dismiss' |undefined;
+type Accessory = 'menu' | 'external' | 'dismiss' | undefined;
 
+/**
+ * Returns the appropriate icon accessory based on the expected behavior.
+ * 
+ * @param {Accessory} behavior - enum representing how the component is meant to behave.
+ * @returns {ReactElement} - an `<icon/>` element
+ */
 function getAccessory(behavior: Accessory) {
     switch (behavior) {
         case 'menu':
@@ -34,11 +40,26 @@ export type ButtonProps = (React.ButtonHTMLAttributes<HTMLButtonElement>
         icon?: string,
     };
 
+/**
+ * Determines if this should render as an icon-only element.
+ * 
+ * @param {ButtonProps} props - Configuration object
+ * @param {String} [props.icon] - String identifier for an icon
+ * @param {ReactNode} [props.children] - Child elements
+ * @param {Accessory} [props.behavior] - The expected behavior of the element
+ * @returns {Boolean} - Determines if the icon should render or not
+ */
 function iconOnly({ icon, children, behavior }: ButtonProps): boolean {
     if (children) return false;
     return !Boolean(icon && behavior);
 }
 
+/**
+ * Creates a `<Button/>` component
+ * 
+ * @param {ButtonProps} props - Component configuration object
+ * @returns {ReactElement} - A button component
+ */
 export const Button = forwardRef<HTMLElement, ButtonProps>(({
     behavior,
     children,
@@ -62,14 +83,17 @@ export const Button = forwardRef<HTMLElement, ButtonProps>(({
         square: square || iconOnly({ icon: iconRef, children, behavior })
     });
 
+    // Configure ARIA from given props.
     const ariaLabelledby = [props['aria-labelledby']];
     if (behavior === 'dismiss') ariaLabelledby.push(IDREF.close);
     config['aria-labelledby'] = ariaLabelledby.filter(Boolean).join(' ') || undefined;
 
+    // If this is meant as a menu item, align content to the starting edge.
     if (['option', 'menuitem'].includes(role as string)) {
         config.placeChildren = 'start'
     }
 
+    // Warn if given props would make component inaccessible.
     if (!children && !(config['aria-label'] || config['aria-labelledby'])) {
         console.warn(`
             No children were provided,
