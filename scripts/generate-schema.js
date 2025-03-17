ed63a0fe6d418e64faedc40cbed3a1eb41216073
Donnie D'Amato <fauxserious@gmail.com>
2025-03-17 00:01:03 -0400

    Use intents.yml

--- scripts/generate-schema.js
+++ scripts/generate-schema.js
@@ -1,56 +1,56 @@
-import { type } from 'os';
-import {
-    PRIORITY,
-    PROPERTY_COLOR,
-    PROPERTY_FONT,
-    PROPERTY_SPACE
-} from './properties.js';
+import fs from 'fs';
+import path from 'path';
+import yaml from 'js-yaml';
 
-function final(property) {
-    const base = {
-        type: 'object',
-        additionalProperties: false,
-        required: ['$value'],
-        properties: {
-            $value: {
-                type: ['number', 'string']
-            }
-        }
+const INTENTS_YAML_PATH = path.join(process.cwd(), 'scripts', 'intents.yml');
+const intents = yaml.load(fs.readFileSync(INTENTS_YAML_PATH, 'utf-8'));
+
+function typeofValue(key) {
+    const base = { $value: { type: ['number', 'string'] } };
+
+    if (key.includes('fontFamily')) {
+        base.$fallback = { type: ['string'] }
     }
 
-    if (property.includes('Color')) {
-        base.properties.$influence = { type: ['number'] }
-    }
-
-    if (property.includes('fontFamily')) {
-        base.properties.$fallback = { type: ['string'] }
+    if (key.includes('Color')) {
+        base.$influence = { type: ['number'] }
     }
 
     return base;
 }
 
-function permutate(...arrays) {
-    const host = {};
-
-    const recurse = (acc, remaining) => {
-        const [next, ...rest] = remaining;
-        next.forEach((key) => {
-            acc[key] = final(key);
-            if (rest.length) {
-                acc[key].type = 'object';
-                acc[key].additionalProperties = false;
-                if (rest[0]) {
-                    acc[key].required = rest[0];
-                }
-                acc[key].properties = {};
-                recurse(acc[key].properties, rest);
+function createTokens() {
+    return intents.reduce((acc, path) => {
+        return path.split('.').reduce((property, key, index, keys) => {
+    
+            // If key on property doesn't exist, create it
+            if (!property[key]) {
+                Object.assign(property, { [key]: {
+                    type: 'object',
+                    additionalProperties: false,
+                    required: [],
+                    properties: {}
+                } });
             }
-        });
-    }
-
-    recurse(host, arrays);
-    return host;
+    
+            // Add the following key as required
+            const required = keys[index + 1] || '$value';
+            if (!property[key].required.includes(required)) {
+                property[key].required.push(required);
+            }
+    
+            // If we're at the end, set the $value schema and return collection
+            if (path.endsWith(key)) {
+                Object.assign(property[key].properties, typeofValue(key));
+                return acc;
+            }
+    
+            // If not at the end, continue looping
+            return property[key].properties;
+        }, acc);
+    }, {});
 }
+
 
 export default function main() {
     return {
@@ -70,13 +70,8 @@ export default function main() {
             tokens: {
                 type: 'object',
                 properties: {
-                    ...permutate(['surface'], PRIORITY, PROPERTY_COLOR),
-                    ...permutate(['action'], PRIORITY, PROPERTY_COLOR),
-                    ...permutate(['control'], PROPERTY_COLOR),
-                    ...permutate(['text'], PRIORITY, PROPERTY_FONT),
-                    ...permutate(['space'], PROPERTY_SPACE),
+                    ...createTokens(),
                 }
-                
             }
         }
     }
